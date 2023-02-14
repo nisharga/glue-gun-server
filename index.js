e04163d46660ba789670a38ccce5895778c87f16
Nisharga Kabir <kabirnisharga@gmail.com>
2023-02-14 13:51:38 +0600

    env in url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.qemdz.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://Nisharga:aDj8QSwONIMYsWtK@cluster0.qemdz.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
