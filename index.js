ef7b75937c42fe664d0855332e69c812346a889a
Nisharga Kabir <kabirnisharga@gmail.com>
2023-02-14 13:02:20 +0600

    nodemon fixed for cyclic

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 });
 
 app.get("/", (req, res) => {
-  res.send("Server is Running Cool");
+  res.send("New Server is Running Cool");
 });
 
 app.get("/maa", (req, res) => {
